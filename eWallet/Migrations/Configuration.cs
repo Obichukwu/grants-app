7f2712bc68f4837944d5bf94e397fa62048c1ec0
Obichukwu Onyeowuzoni <obichukwu.zoni@outlook.com>
2017-07-26 03:36:58 +0100

    Move using in Configuration

--- eWallet/Migrations/Configuration.cs
+++ eWallet/Migrations/Configuration.cs
@@ -1,13 +1,14 @@
+using eWallet.Models;
+using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.EntityFramework;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Data.Entity.Migrations;
+using System.Linq;
+
 namespace eWallet.Migrations
 {
-    using Microsoft.AspNet.Identity;
-    using Microsoft.AspNet.Identity.EntityFramework;
-    using Models;
-    using System;
-    using System.Collections.Generic;
-    using System.Data.Entity;
-    using System.Data.Entity.Migrations;
-    using System.Linq;
 
     internal sealed class Configuration : DbMigrationsConfiguration<eWallet.Models.eWalletContext>
     {
